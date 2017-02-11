48e4055094363a9d3c600fbb24043dbaad606c55
delphifirst <delphifirst@foxmail.com>
2017-02-11 20:50:42 +0800

    Add shader for particle system. Add particle system init code

--- D3D11Playground/scene_objects/particle_system.cpp
+++ D3D11Playground/scene_objects/particle_system.cpp
@@ -1,5 +1,8 @@
 #include "particle_system.h"
 
+#include <vector>
+#include <cmath>
+
 using namespace std;
 using namespace DirectX;
 
@@ -8,5 +11,27 @@ ParticleSystem::ParticleSystem(const wstring& name, const wstring& texture_filen
 	float min_life_time, float max_life_time, int particle_num)
 	: SceneObject(name)
 {
-	
+	for (int i = 0; i < particle_num; ++i)
+	{
+		Particle new_particle;
+		XMStoreFloat3(&new_particle.position, spawn_pos);
+		XMStoreFloat3(&new_particle.speed_direction, RandomDirection());
+		new_particle.speed = RandomInRange(min_speed, max_speed);
+		new_particle.life_time = RandomInRange(min_life_time, max_life_time);
+		new_particle.current_life_time = 0;
+		particles_.push_back(new_particle);
+	}
+}
+
+float ParticleSystem::RandomInRange(float min_val, float max_val)
+{
+	return min_val + (max_val - min_val) * random(random_engine);
+}
+
+XMVECTOR XM_CALLCONV ParticleSystem::RandomDirection()
+{
+	float theta = RandomInRange(0, XM_2PI);
+	float z = RandomInRange(-1, 1);
+	float r = sqrt(1 - z * z);
+	return XMVectorSet(r * cos(theta), r * sin(theta), z, 0);
 }
--- D3D11Playground/scene_objects/particle_system.h
+++ D3D11Playground/scene_objects/particle_system.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <random>
 
 #include <d3d11.h>
 #include <DirectXMath.h>
@@ -10,7 +11,37 @@
 class ParticleSystem : public SceneObject
 {
 public:
+	struct MatrixBuffer
+	{
+		DirectX::XMFLOAT4X4 world_matrix;
+		DirectX::XMFLOAT4X4 view_matrix;
+		DirectX::XMFLOAT4X4 proj_matrix;
+	};
+
+	struct Vertex
+	{
+		DirectX::XMFLOAT3 position;
+		DirectX::XMFLOAT2 tex_coord;
+	};
+
+	struct Particle
+	{
+		DirectX::XMFLOAT3 position;
+		DirectX::XMFLOAT3 speed_direction;
+		float speed;
+		float life_time;
+		float current_life_time;
+	};
+
 	ParticleSystem(const std::wstring& name, const std::wstring& texture_filename,
 		DirectX::FXMVECTOR spawn_pos, float min_speed, float max_speed,
 		float min_life_time, float max_life_time, int particle_num);
+
+private:
+	float RandomInRange(float min_val, float max_val);
+	DirectX::XMVECTOR XM_CALLCONV RandomDirection();
+	std::default_random_engine random_engine;
+	std::uniform_real_distribution<float> random;
+
+	std::vector<Particle> particles_;
 };
